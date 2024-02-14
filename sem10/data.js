757271d5470047085bb63ec1b8d226ff7a179c31
Vit <torrvick@gmail.com>
2024-02-14 22:04:58 +0300

    Seminar 10 done

--- sem10/data.js
+++ sem10/data.js
@@ -59,3 +59,63 @@ data.forEach((element) => {
 		propEl.append(propValueEl);
 	}
 });
+
+document.addEventListener('click', function (e) {
+	const target = e.target;
+	const targetDivEl = target.closest('.recipe');
+	console.log(target);
+	if (targetDivEl) {
+		const recipeId = targetDivEl.dataset.id;
+		const recipe = data.find((recipe) => recipe.id == recipeId);
+		console.log(recipe);
+
+		const modalEl = document.createElement('div');
+		modalEl.classList.add('modal');
+		modalEl.style.display = 'block';
+		mainContentEl.append(modalEl);
+
+		const modalBoxEl = document.createElement('div');
+		modalBoxEl.classList.add('modal-box');
+		modalEl.append(modalBoxEl);
+
+		const modalContentEl = document.createElement('div');
+		modalContentEl.classList.add('modal-content');
+		modalBoxEl.append(modalContentEl);
+
+		const imgEl = document.createElement('img');
+		imgEl.classList.add('modal-img');
+		imgEl.src = recipe.image;
+		imgEl.alt = recipe.name;
+		modalContentEl.append(imgEl);
+
+		const ingredientsTextEl = document.createElement('p');
+		ingredientsTextEl.textContent = `To cook this ${recipe.name}, you must buy the following products:`;
+		modalContentEl.append(ingredientsTextEl);
+
+		const ingridientsEl = document.createElement('ul');
+		modalContentEl.append(ingridientsEl);
+		const ingredients = recipe.ingredients;
+		ingredients.forEach((ingridient) => {
+			const liEl = document.createElement('li');
+			liEl.textContent = ingridient;
+			ingridientsEl.append(liEl);
+		});
+
+		const instructionsTextEl = document.createElement('p');
+		instructionsTextEl.textContent = `And do the following steps:`;
+		modalContentEl.append(instructionsTextEl);
+
+		const instructionsEl = document.createElement('ul');
+		modalContentEl.append(instructionsEl);
+		const instructions = recipe.instructions;
+		instructions.forEach((instruction) => {
+			const liEl = document.createElement('li');
+			liEl.textContent = instruction;
+			instructionsEl.append(liEl);
+		});
+	}
+
+	if (target.classList.contains('modal')) {
+		target.style.display = 'none';
+	}
+});
