4ab59688fcfa80461bd776bf1bfe53abd3c80523
Vit <torrvick@gmail.com>
2024-02-22 20:22:44 +0300

    Seminar 12 done

---
+++ sem12/scripts/data.js
@@ -0,0 +1,56 @@
+export const productsJSON = `[
+{
+    "id": 1,
+    "brand": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "currency": "USD",
+    "price": 222,
+    "color": "Red",
+    "photo": "assets/img/product1.png"
+},
+{
+    "id": 2,
+    "brand": "BOTTEGA VENETA",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "currency": "USD",
+    "price": 245,
+    "color": "Red",
+    "photo": "assets/img/product2.png"
+},
+{
+    "id": 3,
+    "brand": "SAINT LAURENT",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "currency": "RUB",
+    "price": 208,
+    "color": "Red",
+    "photo": "assets/img/product3.png"
+},
+{
+    "id": 4,
+    "brand": "VALENTINO",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "currency": "USD",
+    "price": 115,
+    "color": "Red",
+    "photo": "assets/img/product4.png"
+},
+{
+    "id": 5,
+    "brand": "HUGO BOSS",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "currency": "USD",
+    "price": 234,
+    "color": "Red",
+    "photo": "assets/img/product5.png"
+},
+{
+    "id": 6,
+    "brand": "MICHAEL KORS",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "currency": "RUB",
+    "price": 522,
+    "color": "Red",
+    "photo": "assets/img/product6.png"
+}
+]`;
